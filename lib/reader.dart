d3483a3b7fa6a8dc3c849e2ea55fe9725559319e
HallowedSteel <warman643@gmail.com>
2023-03-28 21:58:59 +0300

    finsihed reading/writing/lock

--- lib/reader.dart
+++ lib/reader.dart
@@ -81,20 +81,16 @@ class MyAppNFCState extends State<MyAppNFC> {
   void _ndefWrite() {
     NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
       var ndef = Ndef.from(tag);
+
       if (ndef == null || !ndef.isWritable) {
         result.value = 'Tag is not ndef writable';
         NfcManager.instance.stopSession(errorMessage: result.value);
         return;
       }
-
       NdefMessage message = NdefMessage([
         NdefRecord.createText('Hello World!'),
-        NdefRecord.createUri(Uri.parse('https://flutter.dev')),
-        NdefRecord.createMime(
-            'text/plain', Uint8List.fromList('Hello'.codeUnits)),
-        NdefRecord.createExternal(
-            'com.example', 'mytype', Uint8List.fromList('mydata'.codeUnits)),
       ]);
+
 
       try {
         await ndef.write(message);
